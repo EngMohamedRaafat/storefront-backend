a3b880eac53c1dcea038306656ceee32ca19c577
EngMohamedRaafat <muhammedraafat@outlook.com>
2022-03-22 09:18:50 +0200

    feat: Add username to the user table

--- migrations/sqls/20220318113544-users-table-up.sql
+++ migrations/sqls/20220318113544-users-table-up.sql
@@ -1,5 +1,6 @@
 CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
     firstname VARCHAR(50) NOT NULL,
     lastname VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
